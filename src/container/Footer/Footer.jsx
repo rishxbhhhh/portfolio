1fd7c60ccb65d29c360781caedff7ff6e4cd942e
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-07-02 22:53:59 +0530

    added copyright and social to footer only

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { images } from '../../constants';
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
-import './Footer.scss';
-
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
+import "./Footer.scss";
+import {motion} from 'framer-motion';
+import { BsGithub, BsLinkedin, BsInstagram } from "react-icons/bs";
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -21,13 +26,14 @@ const Footer = () => {
     setLoading(true);
 
     const contact = {
-      _type: 'contact',
+      _type: "contact",
       name: formData.username,
       email: formData.email,
       message: formData.message,
     };
 
-    client.create(contact)
+    client
+      .create(contact)
       .then(() => {
         setLoading(false);
         setIsFormSubmitted(true);
@@ -37,25 +43,45 @@ const Footer = () => {
 
   return (
     <>
-      <h2 className="head-text">Say <span> Hello </span>to Me</h2>
+      <h2 className="head-text">
+        Say <span> Hello </span>to Me
+      </h2>
 
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:rajpurohitrishabh1@gmail.com" className="p-text">rajpurohitrishabh1@gmail.com</a>
+          <a href="mailto:rajpurohitrishabh1@gmail.com" className="p-text">
+            rajpurohitrishabh1@gmail.com
+          </a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+91 8929566279" className="p-text">+91 8929566279</a>
+          <a href="tel:+91 8929566279" className="p-text">
+            +91 8929566279
+          </a>
         </div>
       </div>
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+            <input
+              className="p-text"
+              type="text"
+              placeholder="Your Name"
+              name="username"
+              value={username}
+              onChange={handleChangeInput}
+            />
           </div>
           <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+            <input
+              className="p-text"
+              type="email"
+              placeholder="Your Email"
+              name="email"
+              value={email}
+              onChange={handleChangeInput}
+            />
           </div>
           <div>
             <textarea
@@ -66,21 +92,46 @@ const Footer = () => {
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {!loading ? "Send Message" : "Sending..."}
+          </button>
         </div>
       ) : (
         <div>
-          <h3 className="head-text">
-            Thank you for getting in touch!
-          </h3>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
       )}
+
+      <div id='app__social' style={{display: 'flex',flexDirection: 'row'}}>
+        <a href="https://www.instagram.com/rishabh941._/">
+          <motion.div className="app__social-icon">
+            <BsInstagram />
+          </motion.div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/rishabhrajpurohit1/">
+          <motion.div className="app__social-icon">
+            <BsLinkedin />
+          </motion.div>
+        </a>
+
+        <a href="https://www.github.com/rishxbhhhh">
+          <motion.div className="app__social-icon">
+            <BsGithub />
+          </motion.div>
+        </a>
+      </div>
+
+      <div className="copyright">
+        <p className="p-text">&copy; Rishabh Rajpurohit 2022</p>
+        <p className="p-text">Made with ❤️ & React JS</p>
+      </div>
     </>
   );
 };
 
 export default AppWrap(
-  MotionWrap(Footer, 'app__footer'),
-  'contact',
-  'app__whitebg',
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
 );
--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { NavigationDots, SocialMedia } from '../components';
+import React from "react";
+import { NavigationDots, SocialMedia } from "../components";
 
-
-const AppWrap = (Component,idName,classNames) => function HOC() {
-  return (
-    <div id={idName} className={`app__container ${classNames}`} style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: '1rem', width: '100%'}}>
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div
+        id={idName}
+        className={`app__container ${classNames}`}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          padding: "1rem",
+          width: "100%",
+        }}
+      >
         <SocialMedia />
         <div className="app__wrapper app__flex">
-            <Component />
-            <div className="copyright">
-                <p className="p-text">&copy; Rishabh Rajpurohit 2022</p>
-                <p className="p-text">Made with ❤️ and REACT-JS</p>
-            </div>
+          <Component />
         </div>
         <NavigationDots active={idName} />
-    </div>
-  );
-}
+      </div>
+    );
+  };
 
 export default AppWrap;
