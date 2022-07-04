83543b6315b9428e58d9e40d05a1e17bcca40f2c
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-07-04 19:38:02 +0530

    Manual Theme Switch without persistence(memory)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,101 @@
-import React from "react";
-// import DarkMode from "./DarkMode";
+import React, { useState, useEffect } from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 const App = () => {
+  function DefaultTheme() {
+    let temp_theme = "light";
+    if (localStorage.getItem("portfolio.theme") !== null) {
+      temp_theme = localStorage.getItem("portfolio.theme");
+    }
+    console.log(`Saved Theme: ${temp_theme}`);
+    return temp_theme;
+  }
+
+  // // switch to saved theme after three seconds
+  //  const temp = () => {
+  //   var executed = false;
+  //   if (!executed) {
+  //     var switchTheme = setTimeout(() => {
+  //       const default__theme = DefaultTheme();
+  //     if (default__theme === "dark") {
+  //       setTheme("dark");
+  //       setThemeIcon(
+  //         <BsFillSunFill className="theme" title="Change Theme to Light" />
+  //       );
+  //       root.setAttribute("color-scheme", "dark");
+  //       console.log("switched to last saved theme!");
+  //     } else if (default__theme === "light") {
+  //       setTheme("light");
+  //       setThemeIcon(
+  //         <BsFillMoonFill className="theme" title="Change Theme to Dark" />
+  //       );
+  //       root.setAttribute("color-scheme", "light");
+  //       console.log("switched to last saved theme!");
+  //     } else {
+  //       setTheme("light");
+  //       setThemeIcon(
+  //         <BsFillMoonFill className="theme" title="Change Theme to Dark" />
+  //       );
+  //       root.setAttribute("color-scheme", "light");
+  //       console.log("switched to default theme!");
+  //     }
+  //     executed=true;
+  //     },2000);
+  //   }
+  //   else{
+  //     clearTimeout(switchTheme);
+  //   }
+  // };
+
+  const [theme, setTheme] = useState('light');
+  const [themeIcon, setThemeIcon] = useState(
+    <BsFillMoonFill className="theme" title="Change Theme to Dark" />
+  );
+  const root = document.querySelector("#root");
+
+  // will update localStorage everytime theme is changed (for persistence)
+  useEffect(() => {
+    localStorage.setItem("portfolio.theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      setThemeIcon(
+        <BsFillSunFill className="theme" title="Change Theme to Light" />
+      );
+      root.setAttribute("color-scheme", "dark");
+      console.log("theme set to dark!");
+      console.log("themeIcon set to Sun!");
+    } else {
+      setTheme("light");
+      setThemeIcon(
+        <BsFillMoonFill className="theme" title="Change Theme to Dark" />
+      );
+      root.setAttribute("color-scheme", "light");
+      console.log("theme set to light!");
+      console.log("themeIcon set to Moon!");
+    }
+  };
+
+  //for automatic theme change (not required at the moment)
+  // useEffect(() => {
+  //   if (
+  //     window.matchMedia &&
+  //     window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   ) {
+  //     toggleTheme();
+  //   } else {
+  //     toggleTheme();
+  //   }
+  // },[]);
 
   return (
     <div className="app">
-      <Navbar />
+      <Navbar themeIcon={themeIcon} toggleTheme={toggleTheme} />
       <Header />
       <About />
       <Work />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,35 +4,55 @@ import { motion } from "framer-motion";
 import "./Navbar.scss";
 import { images } from "../../constants";
 
-function Navbar() {
+function Navbar(props) {
   const [toggle, setToggle] = useState(false);
+  function ThemeIcon() {
+    return (
+      <button className="theme" onClick={props.toggleTheme}>
+        {props.themeIcon}
+      </button>
+    );
+  }
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ThemeIcon />
       <ul className="app__navbar-links">
-        {["home", "about", "work", "skills", "testimonials","contact"].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
+        {["home", "about", "work", "skills", "testimonials", "contact"].map(
+          (item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
       </ul>
       
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
         {toggle && (
           <motion.div
-            whileInView={{width:[0,300], opacity:[0.8,1]}}
-            transition={{duration: 0.5, ease: "easeOut" }}
+            whileInView={{ width: [0, 300] }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
+
             <ul>
-              {["home", "about", "work", "skills", "testimonials","contact"].map((item) => (
+              {[
+                "home",
+                "about",
+                "work",
+                "skills",
+                "testimonials",
+                "contact",
+              ].map((item) => (
                 <li key={item}>
-                  <a href={`#${item}`} onClick={() => setInterval(setToggle(false),800)}>
+                  <a
+                    href={`#${item}`}
+                    onClick={() => setInterval(setToggle(false), 800)}
+                  >
                     {item}
                   </a>
                 </li>
