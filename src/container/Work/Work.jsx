43bee01163c2c88c8c377b54a53e37741f1efdfb
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-07-09 20:52:44 +0530

    changed work categories plus minor changes

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ function Work() {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+        {["UI/UX", "Web App","Vanilla JS", "Node JS", "React JS", "All"].map(
           (item, index) => (
             <div
               key={index}
